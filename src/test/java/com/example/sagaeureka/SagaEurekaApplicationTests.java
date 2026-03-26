d2a39382adf41dbc690d26718a093b13f8445e4a
YeonSM <100843591+yeonseungmin@users.noreply.github.com>
2026-03-26 14:50:42 +0900

    Actions

--- src/test/java/com/example/sagaeureka/SagaEurekaApplicationTests.java
+++ src/test/java/com/example/sagaeureka/SagaEurekaApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.sagaeureka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@Slf4j
+@ActiveProfiles("test")
 @SpringBootTest
 class SagaEurekaApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("gateway 실행 성공");
     }
 
 }
